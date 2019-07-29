cbab6dc450906ffcbd1503b61bc9d71e7c2222b0
wpmjones@gmail.com <8751ImpalaMustang!>
2019-07-29 16:44:14 -0400

    more setup

--- cogs/guildconfig.py
+++ cogs/guildconfig.py
@@ -1,4 +1,7 @@
+import discord
+import coc
 from discord.ext import commands
+
 
 class GuildConfig(commands.Cog):
     def __init__(self, bot):
@@ -7,9 +10,41 @@ class GuildConfig(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         """Event listener which is called when the bot joins a server."""
-        # TODO INSERT guild.name, guild.id into database
-        pass
+        sql = "INSERT INTO guilds (guild_id, guild_name) VALUES ($1, $2) ON CONFLICT (guild_id) DO NOTHING"
+        await self.bot.pool.execute(sql, guild.id, guild.name)
+        welcome_msg = ("Thank you for installing PushBot! My job is to help you track your trophy push event! "
+                       "My prefix is `/`. To get started, how about telling me which clans you'd like to include "
+                       "in your event.  Use the `/add` command to add one or more clan tags (separated by comma).")
+        if guild.system_channel:
+            try:
+                await guild.system_channel.send(welcome_msg)
+                return
+            except (discord.Forbidden, discord.HTTPException):
+                pass
+        for channel in guild.channels:
+            if not isinstance(channel, discord.TextChannel):
+                continue
+            if channel.permissions_for(channel.guild.get_member(self.bot.user_id)).send_messages:
+                try:
+                    await channel.send(welcome_msg)
+                except (discord.Forbidden, discord.HTTPException):
+                    pass
+                return
 
     @commands.command(name="add")
-    async def add_item(self, ctx, tag: str):
+    async def add_clan(self, ctx, tag: str):
         """Add clan or player to database."""
+        tag = coc.utils.correct_tag(tag)
+        sql = "SELECT * FROM claims WHERE clan_tag = $1 AND guild_id = $2"
+        fetch = await ctx.db.fetch(sql, tag, ctx.guild.id)
+        if fetch:
+            raise commands.BadArgument("This clan is already linked to your server")
+        try:
+            clan = await ctx.coc.get_clan(tag)
+        except coc.NotFound:
+            raise commands.BadArgument("I can't find a clan with the tag: {tag}")
+        sql = "INSERT INTO claims (guild_id, clan_tag) VALUES ($1, $2)"
+        await self.bot.pool.execute(sql, ctx.guild.id, tag)
+        await ctx.send(f"{clan.name} ({clan.tag}) added to the database.")
+        # TODO Do I need to add players at this point? Maybe not. Event start instead.
+        # TODO Time adding players vs updating trophies
